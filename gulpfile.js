c2d05c3b62d029208ddd785f6389e0975ead718a
= <=>
2019-03-21 09:54:48 +0600

    b-4.3.1 supported

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass  = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+
+
+gulp.task('sass', function () {
+    return gulp.src('./assets/scss/style.scss')
+      .pipe(sass().on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('./'));
+});
+
+// Watch Task
+gulp.task('watch', function() {
+     gulp.watch('./assets/scss/**/*.scss', gulp.series('sass'));
+ });
