2bc13f07cda2def0c4e4777906e1f9b23464c8d7
= <=>
2019-03-21 10:26:37 +0600

    gulp file updated

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function () {
     return gulp.src('./assets/scss/style.scss')
+      .pipe(sourcemaps.init())
       .pipe(sass().on('error', sass.logError))
       .pipe(sourcemaps.write())
       .pipe(gulp.dest('./'));
